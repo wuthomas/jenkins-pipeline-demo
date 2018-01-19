e74c416aef66a8f44260f4c74645db5a51722a1b
wuthomas <35551041+wuthomas@users.noreply.github.com>
2018-01-19 17:25:45 +0800

    Update test_flask.py

--- test/test_flask.py
+++ test/test_flask.py
@@ -1,6 +1,8 @@
 import unittest
+
+
 class TestFlask(unittest.TestCase):
-def test_response(self):
-pass
-def test_request(self):
-pass
+  def test_response(self):
+    pass
+  def test_request(self):
+    pass
