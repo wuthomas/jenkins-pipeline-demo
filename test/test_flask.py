8702492a4a4ee9cb93d62ed4701814a92fd9cf2e
wuthomas <35551041+wuthomas@users.noreply.github.com>
2018-01-19 18:35:32 +0800

    Update test_flask.py

--- test/test_flask.py
+++ test/test_flask.py
@@ -1,7 +1,8 @@
 import unittest
 
 class TestFlask(unittest.TestCase):
-  def self.fail('cannot get response')
+  def test_response(self):
+    self.fail('cannot get response')
     
   def test_request(self):
     pass
